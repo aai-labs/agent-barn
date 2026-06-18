a2f0a76d176f77b9581cfe81abfd3a0919d47dfa
Andriy852 <andrijzurba79@gmail.com>
2026-06-18 19:05:22 +0300

    AF-117_agent_skills_crud: fix linter error

--- api/migrations/versions/fd1b1a0bdd6e_merge_skills_and_templates_migrations.py
+++ api/migrations/versions/fd1b1a0bdd6e_merge_skills_and_templates_migrations.py
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
 revision: str = "fd1b1a0bdd6e"
-down_revision: Union[str, None] = ("032ff9a474bb", "c2d3e4f5a6b7")
+down_revision: Union[str, tuple[str, ...], None] = ("032ff9a474bb", "c2d3e4f5a6b7")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
