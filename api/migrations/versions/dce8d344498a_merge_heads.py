790d8db5fa0433ab35ca77368f990f4eeb2158eb
Kalkidan Betre <kalkidan@aai-labs.com>
2026-07-23 14:11:47 +0300

    Create dce8d344498a_merge_heads.py

---
+++ api/migrations/versions/dce8d344498a_merge_heads.py
@@ -0,0 +1,23 @@
+"""merge heads
+
+Revision ID: dce8d344498a
+Revises: a6f2c9d18e47, e1f2a3b4c5d6
+Create Date: 2026-07-23 14:11:36.228894
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "dce8d344498a"
+down_revision: Union[str, None] = ("a6f2c9d18e47", "e1f2a3b4c5d6")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
