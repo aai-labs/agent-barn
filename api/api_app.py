e7c3beae2a32bd9c1797bbd14527e35c5f5b8439
Kalkidan Betre <kalkidan@aai-labs.com>
2026-07-31 21:18:28 +0300

    TEST: trigger api-only change detection

--- api/api_app.py
+++ api/api_app.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager  # noqa: I001
 from typing import Annotated
 
 from fastapi import FastAPI, HTTPException, Response, status
